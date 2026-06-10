d16559fd58a1ea5244cb9c37946072210f85798a
guilleramon <guillerminaramon@hotmail.com.ar>
2026-06-10 13:36:11 -0300

    Agrego gráfico de Pclass

---
+++ scripts/grafico.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+datos = pd.read_csv("titanic.csv")
+
+fig, ax = plt.subplots()
+sns.countplot(x = "Sex", hue = "Survived", data = datos)
+fig.savefig("plot.png")
+
+fig, ax = plt.subplots()
+sns.countplot(x = "Pclass", hue = "Survived", data = datos)
+fig.savefig("plot2.png")
